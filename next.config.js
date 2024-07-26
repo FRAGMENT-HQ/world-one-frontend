8f7a8bac9ac59a8ef7357017f07bf8f51d4ccf4e
unknown <pranav_a@ece.iitr.ac.in>
2024-07-26 15:07:17 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  output: 'export',
-  distDir: 'output',
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production'
-  },
-  images: {
-    unoptimized: true // You may need this if there's any relevant error occurring while building.
-  },
+  // output: 'export',
+  // distDir: 'output',
+  // compiler: {
+  //   removeConsole: process.env.NODE_ENV === 'production'
+  // },
+  // images: {
+  //   unoptimized: true // You may need this if there's any relevant error occurring while building.
+  // },
 
   eslint: {
     // Warning: This allows production builds to successfully complete even if
